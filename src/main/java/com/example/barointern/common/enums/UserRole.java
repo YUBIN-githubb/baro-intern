ad8946aa8f62b86ff5a1177948d22c77028d4ea3
Moon YuBin <yubin5744@gmail.com>
2025-06-10 11:53:14 +0900

    feat : User 클래스 구현

---
+++ src/main/java/com/example/barointern/common/enums/UserRole.java
@@ -0,0 +1,9 @@
+package com.example.barointern.common.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/example/barointern/domain/User/User.java
@@ -0,0 +1,19 @@
+package com.example.barointern.domain.User;
+
+import com.example.barointern.common.enums.UserRole;
+import lombok.Getter;
+
+@Getter
+public class User {
+    private Long id;
+    private String email;
+    private String password;
+    private UserRole userRole;
+
+    public User(Long id, String email, String password, UserRole userRole) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.userRole = userRole;
+    }
+}
